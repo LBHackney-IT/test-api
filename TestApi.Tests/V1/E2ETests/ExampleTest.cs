d435a616e17a75aa582a085afe32fe151b88bd87
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-03-24 12:11:51 +0000

    Rename files & references

---
+++ TestApi.Tests/V1/E2ETests/ExampleTest.cs
@@ -0,0 +1,8 @@
+namespace TestApi.Tests.V1.E2ETests
+{
+    //For guidance on writing integration tests see the wiki page https://github.com/LBHackney-IT/lbh-test-api/wiki/Integration-Tests
+    public class ExampleTest : IntegrationTests<Startup>
+    {
+
+    }
+}
